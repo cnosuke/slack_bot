eca101d4d509face739cf44ef660ee41493f56fe
Shinnosuke TAKEDA <shinnosuke@gmail.com>
2014-03-30 13:42:54 +0900

    Fix bugs

--- filter/test_bot.rb
+++ filter/test_bot.rb
@@ -1,7 +1,12 @@
 module Filter
-  class TestBot
+  class ExampleBot
+    # This is Example script
     def update(p)
-      return "updated! text:#{p['text']}"
+      return nil # If you want to enable this script, remove this line.
+      {
+        username: 'ExampleBot',
+        text: "Hello, #{p['user_name']}! your text is '#{p['text']}'"
+      }
     end
   end
 end
--- server.rb
+++ server.rb
@@ -6,11 +6,22 @@ SLACK_TOKEN = ENV['SLACK_TOKEN']
 puts "TOKEN: #{SLACK_TOKEN}"
 
 before '/hooks' do
-  halt 403, "Token is invalid\n" unless token_valid?
+  unless token_valid?
+    puts 'Token invalid'
+    halt 403, "Token is invalid\n"
+  end
+  if comment_by_myself?
+    puts 'Comment by myself'
+    halt 200
+  end
 end
 
 def token_valid?
   params['token'] == SLACK_TOKEN
+end
+
+def comment_by_myself?
+ params['user_name'] == 'slackbot'
 end
 
 get '/' do
@@ -18,9 +29,11 @@ get '/' do
 end
 
 post '/hooks' do
-  response = filter.update(params)
+  response = filter.update(params).compact.first
   return 200 unless response
-  response.to_json
+  j = response.to_json
+  puts j
+  j
 end
 
 def filter
