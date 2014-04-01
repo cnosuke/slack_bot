05f5eaab0aec711ed280ab401fc78cbe81666959
Shinnosuke TAKEDA <shinnosuke@gmail.com>
2014-04-01 10:31:46 +0900

    Add tiqav_bot and weather_bot

---
+++ filter/tiqav_bot.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'open-uri'
+TIQAV_API = 'http://api.tiqav.com/search.json?q='
+module Filter
+  class TiqavBot
+    def update(p)
+      t = p['text']
+      return nil unless t =~ /^\:tiqav/
+      query = t.split(/\s/)[1..-1].join(' ')
+      { username: 'tiqav', text: "#{query}: #{get_image_url(query)}" }
+    end
+    def get_image_url(query)
+      j = JSON.parse(open(TIQAV_API+URI.encode(query)).read).first(5).sample
+      "http://tiqav.com/#{j['id']}.#{j['ext']}"
+    end
+  end
+end
---
+++ filter/weather_bot.rb
@@ -0,0 +1,34 @@
+WEATHER_API = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'.freeze
+require 'json'
+require 'open-uri'
+module Filter
+  class WeatherBot
+    def update(p)
+      return nil unless p['text'] =~ /^\:weather/
+      {
+        username: "Weather",
+        text: forecast
+      }
+    end
+    def weather
+      @weather ||= JSON.parse(open(WEATHER_API).read)
+    end
+    def forecast
+      f = weather['forecasts']
+      "...\n" + f.map{ |e| forecast_day(e) }.join
+    end
+    def forecast_day(d)
+      str =<<EOB
+#{d["dateLabel"]}(#{d["date"]}): #{d["telop"]}
+  温度: #{temperature(d["temperature"])}
+EOB
+    end
+    def temperature(d)
+      d.map{ |k,v|
+        if v
+          "#{k}: #{v['celsius']}度"
+        end
+      }.compact.join(", ")
+    end
+  end
+end
