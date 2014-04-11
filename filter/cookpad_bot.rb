bdfd868775f3441d2ac31cb00fe0cc7318b4a473
Shinnosuke TAKEDA <shinnosuke@gmail.com>
2014-04-11 13:34:48 +0900

    Add cookpad filter

---
+++ filter/cookpad_bot.rb
@@ -0,0 +1,13 @@
+require 'json'
+require 'open-uri'
+COOKPAD_API = 'http://cookpad.com/s/post?keyword='
+module Filter
+  class CookpadBot
+    def update(p)
+      t = p['text']
+      return nil unless t =~ /^\:cookpad\ /
+      query = t.split(/\s/)[1..-1].join(' ')
+      { username: 'COOKPAD', text: "#{query}: #{COOKPAD_API+URI.encode(query)}" }
+    end
+  end
+end
