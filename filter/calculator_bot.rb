9d16ec695abc0068846f841990709ad28243c8a8
Shinnosuke TAKEDA <shinnosuke@gmail.com>
2014-04-15 12:36:55 +0900

    Add math lib to calculator

--- filter/calculator_bot.rb
+++ filter/calculator_bot.rb
@@ -1,5 +1,6 @@
 module Filter
   class CalculatorBot
+    include Math
     def update(p)
       t = p['text']
       re = /^\:= /
@@ -15,7 +16,10 @@ module Filter
     end
 
     def validate(exp)
-      !!(exp =~ /^(\d|\*|\+|\-|\/|\.|\ )+$/)
+      !!(exp =~ /^(\d|\*|\+|\-|\/|\.|\ |\(|\)|#{math_re})+$/)
+    end
+    def math_re
+      %w(acos acosh asin asinh atan atan2 atanh cbrt cos cosh erf erfc exp frexp gamma hypot ldexp lgamma log log10 log2 rsqrt sin sinh sqrt sqrt tan tanh PI E).join("|")
     end
   end
 end
