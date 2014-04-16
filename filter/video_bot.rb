136e2248254e64a99eb595808e88d440d3f45ab8
Shinnosuke Takeda <shinnosuke@gmail.com>
2014-04-16 23:56:32 +0900

    Add Space

--- filter/video_bot.rb
+++ filter/video_bot.rb
@@ -4,7 +4,7 @@ module Filter
   class VideoBot
     def update(p)
       t = p['text']
-      return nil unless t =~ /^\:video/
+      return nil unless t =~ /^\:video /
       query = t.split(/\s/)[1..-1].join(' ')
       { username: 'Video', text: "#{query}: #{get_video_url(query)}" }
     end
