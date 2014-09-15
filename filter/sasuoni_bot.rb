b155ad80215bb1731306eb83505578ee36266e53
Ryo MURAKAMI <moosan63@gmail.com>
2014-09-16 00:06:24 +0900

    sasuoni 追加

---
+++ filter/sasuoni_bot.rb
@@ -0,0 +1,36 @@
+require 'json'
+require 'open-uri'
+module Filter
+  class SasuoniBot
+    SASUONI_DATA_URI = "http://sasuoni.herokuapp.com/yaml/sasuoni_images.yaml"
+
+    def update(p)
+      t = p['text']
+      return nil unless t =~ /^\:sasuoni/
+      if t =~ /^\:sasuoni list/
+        { username: 'sasuoni', text: fetch_data.map { |z| z["word"] }.uniq.sort.join(", ") }
+      else
+        { username: 'sasuoni', text: find_sasuoni_by_keyword(t.split(/\ /).last)["image"] }
+      end
+    end
+
+    def find_sasuoni_by_keyword(keyword)
+      data = fetch_data
+      if keyword
+        entry = data.select {  |z| z["word"].include?(keyword) }.sample
+      end
+
+      entry || data.sample
+    end
+
+    def fetch_data
+      return @fetched_data if @fetched_data
+      sasuoni_data = open(SASUONI_DATA_URI).read
+      sasuoni_data = sasuoni_data.
+        match(/this.items = (.+?);/m)[1].
+        gsub(/(word|image|src):/, "'\\1':").
+        gsub("'", '"')
+      @fetched_data = JSON.parse(sasuoni_data)
+    end
+  end
+end
